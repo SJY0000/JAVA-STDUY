7c5ee7d4a76a18436de62b3db8b49ab64e8ec731
[sjy] <[tutu8880@naver.com]>
2021-11-17 15:16:00 +0900

    11월 17일

---
+++ HelloWorld/src/Hello/App.java
@@ -0,0 +1,14 @@
+package Hello; // 패키지 헬로우
+
+public class App { // 클래스 App 만들기
+
+	public static void main(String[] args) {
+		// 메인 메소드에서 프로그램 시작
+		// 주석 단출키 CTRL + / 
+		System.out.println("Hello World");
+		// CTRL + F11
+		
+	}
+
+}
+
---
+++ MainMethod/src/application/App.java
@@ -0,0 +1,9 @@
+package application;
+
+public class App {
+	
+	public static void main(String[] args) {
+		// 메인 메소드
+		System.out.println("안녕하세요\n새 자바 프로그램을\n실행 합니다.");
+	}
+}
